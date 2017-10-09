d368d2b70c025a491d4af8f38e0221aada024d38
danielvivacqua <drvivacqua@hotmail.com>
2017-10-09 13:46:47 -0400

    Brought dog into Program class

--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -10,18 +10,38 @@ namespace IntroToOOP
     {
         static void Main(string[] args)
         {
-            Cat mittens = new Cat();
+            //Cat mittens = new Cat();
 
-            mittens.Name = "Mittens";
+            //mittens.Name = "Mittens";
 
-            Cat secondCat = new Cat("Oz", 3, "orange and white");
+            //Cat secondCat = new Cat("Oz", 3, "orange and white");
 
             //Console.WriteLine(mittens.Name);
             //Console.WriteLine(secondCat.Name);
 
-            mittens.Eat();
+            //mittens.Eat();
 
-            mittens.WorkOut();
+            //mittens.WorkOut();
+            Dog firstDoge = new Dog();
+
+            firstDoge.Weight = 60.00;
+            firstDoge.Height = 3.2f;
+            firstDoge.FurLength = "flowing mane";
+            firstDoge.RunningSpeed = "will fetch ... eventually.";
+
+            firstDoge.Groom();
+            Console.WriteLine(firstDoge.FurLength);
+
+            firstDoge.Run();
+            Console.WriteLine("The dog now weighs " + firstDoge.Weight);
+            Console.WriteLine(firstDoge.RunningSpeed);
+
+            firstDoge.Bark();
+
+
+
+
+
 
 
         }

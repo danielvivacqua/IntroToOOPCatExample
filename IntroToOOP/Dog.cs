3cc74ca6175ffb4b659fdbe5b0d775ed0ec4a42b
danielvivacqua <drvivacqua@hotmail.com>
2017-10-09 11:42:37 -0400

    Created dog class

---
+++ IntroToOOP/Dog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Dog
+    {
+        //fields
+        private string furLength;
+        private float height;
+        private string runningSpeed;
+        private int weight;
+
+        //Properties
+        public string FurLength
+        {
+            get { return this.furLength; }
+            set { this.furLength = value; }
+        }
+
+        public float Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public string RunningSpeed
+        {
+            get { return this.runningSpeed; }
+            set { this.runningSpeed = value; }
+        }
+
+        public int Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        //Constructors
+        public Dog()
+        {
+
+        }
+
+        //overloaded Constructor
+        public Dog(string furLength, float height)
+        {
+            this.furLength = furLength;
+            this.height = height;
+        }
+
+    }
+}

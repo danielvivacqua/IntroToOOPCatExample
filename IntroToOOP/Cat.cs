58efaffd1e20c9d8c7762ef52b459f286050a886
danielvivacqua <drvivacqua@hotmail.com>
2017-10-09 10:09:37 -0400

    initial commit

---
+++ IntroToOOP/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Cat
+    {
+        //STATES
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //BEHAVIORS
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor because it takes no 
+            //parameters in the parentheses
+        }
+
+    }
+}
